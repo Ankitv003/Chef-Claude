a1b3a3bcf70fdeb0b30aef56be19b2924f7b0928
Ankit Verma <ankitv78389@gmail.com>
2024-11-28 23:58:37 +0530

    :hammer:[refractor][made input work  in main component.]

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,13 +5,21 @@ const Main = () => {
   const ingredientsListItems = ingredients.map((ingredient) => {
     return <li key={ingredient}>{ingredient}</li>;
   });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new formData(e.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  };
   return (
     <main>
-      <form className="add-ingredients-form">
+      <form onSubmit={handleSubmit} className="add-ingredients-form">
         <input
           type="text"
           placeholder=" e.g. apple"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add Ingredient</button>
       </form>
