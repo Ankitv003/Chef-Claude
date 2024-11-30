0cccb813b4555b77ca4553a6004c9a7f2a53f88b
Ankit Verma <ankitv78389@gmail.com>
2024-11-30 23:54:25 +0530

    :hammer:[refractor][added a new section for ingredient list  in main component.]

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -21,7 +21,19 @@ const Main = () => {
         />
         <button>Add Ingredient</button>
       </form>
-      <ul> {ingredientsListItems}</ul>
+      <section>
+        <h2>Ingredients on hand:</h2>
+        <ul className="ingredients-list" aria-live="polite">
+          {ingredientsListItems}
+        </ul>
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button>Get a recipe</button>
+        </div>
+      </section>
     </main>
   );
 };
