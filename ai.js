a370d0a2edd3e0c9694deabc97a41c9b5fc7f143
Ankit Verma <ankitv78389@gmail.com>
2024-12-29 23:22:38 +0530

    :hammer:[refractor][added code for ai to get recipe, need to configure it.]

--- ai.js
+++ ai.js
@@ -3,8 +3,9 @@ import { HfInference } from "@huggingface/inference";
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `;
-// Make sure you set an environment variable in Scrimba
+// Make sure you set an environment variable in
 // for HF_ACCESS_TOKEN
+// need  to add token value here
 const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
 
 export async function getRecipeFromMistral(ingredientsArr) {
