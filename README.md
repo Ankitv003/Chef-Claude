84862d0b64da9ce9dbf048fcfb7c92475bcb1ffe
Ankit Verma <ankitv78389@gmail.com>
2025-02-17 23:28:49 +0530

    :books:[docs][changed readme.md file]

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Chef Claude App
 - AI integration is last step left and then readme will be added.
 - Final steps will be done by tomorrow and i would be using hugging face instead of claude, so will change the name accordingly , also worked out on some points.
+- need to add api.
--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,26 +1,27 @@
-const IngredientsList = ({ingredients,toggleRecipeShown})=>{
-    const ingredientsListItems = ingredients.map((ingredient) => {
-        return <li key={ingredient}>{ingredient}</li>;
-      });
-    return(
-        ingredients.length > 0 && (
-            <section>
-              <h2>Ingredients on hand:</h2>
-              <ul className="ingredients-list" aria-live="polite">
-                {ingredientsListItems}
-              </ul>
-              {ingredients.length > 3 && (
-                <div className="get-recipe-container">
-                  <div>
-                    <h3>Ready for a recipe?</h3>
-                    <p>Generate a recipe from your list of ingredients.</p>
-                  </div>
-                  <button onClick={toggleRecipeShown}>Get a recipe</button>
-                </div>
-              )}
-            </section>
-        )
+/* eslint-disable react/prop-types */
+const IngredientsList = ({ ingredients, toggleRecipeShown }) => {
+  const ingredientsListItems = ingredients.map((ingredient) => {
+    return <li key={ingredient}>{ingredient}</li>;
+  });
+  return (
+    ingredients.length > 0 && (
+      <section>
+        <h2>Ingredients on hand:</h2>
+        <ul className="ingredients-list" aria-live="polite">
+          {ingredientsListItems}
+        </ul>
+        {ingredients.length > 3 && (
+          <div className="get-recipe-container">
+            <div>
+              <h3>Ready for a recipe?</h3>
+              <p>Generate a recipe from your list of ingredients.</p>
+            </div>
+            <button onClick={toggleRecipeShown}>Get a recipe</button>
+          </div>
+        )}
+      </section>
     )
-}
+  );
+};
 
-export default IngredientsList
+export default IngredientsList;
